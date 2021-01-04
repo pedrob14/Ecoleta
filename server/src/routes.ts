d16a578f625b8c01ee2e5471337ced6e1f882a73
Pedro Henrique <46195037+pedrob14@users.noreply.github.com>
2021-01-04 08:10:40 -0300

    Atualização das rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'})
+routes.get('/items', async (request, response) => {
+    /* await: sempre que for usar uma query para o banco de dados é bom usar await,
+    para aguardar a query terminar, para então ter os resultados. Para usar o await é necessário ter o async */
+    const items = await knex('items').select('*');
+
+    return response.json(items)
 });
 
 export default routes;
