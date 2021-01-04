dd9519f695f7c8ec45ed5e592541d717430eccff
Pedro Henrique <46195037+pedrob14@users.noreply.github.com>
2021-01-03 23:08:58 -0300

    Atualizando as tabelas do banco de dados

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -2,20 +2,14 @@ import Knex from "knex";
 
 export async function up(knex: Knex) {
     // Criar a tabela
-    return knex.schema.createTable('points', table => {
+    return knex.schema.createTable('items', table => {
         table.increments('id').primary();
         table.string('image').notNullable();
-        table.string('name').notNullable();
-        table.string('email').notNullable();
-        table.string('whatsapp').notNullable();
-        table.decimal('latitude').notNullable();
-        table.decimal('longitude').notNullable();
-        table.string('city').notNullable();
-        table.string('uf', 2).notNullable();
+        table.string('title').notNullable();
     });
 }
 
 export async function down(knex: Knex) {
     //Voltar atras / deletar
-    return knex.schema.dropTable('point');
+    return knex.schema.dropTable('items');
 }
