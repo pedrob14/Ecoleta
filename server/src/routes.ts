f025b77f6ec3522c0ee0f49e6527ff688cb8f561
Pedro Henrique <46195037+pedrob14@users.noreply.github.com>
2021-01-04 23:06:54 -0300

    Cadastro de ponto de coleta
    Usando o método POST para a criação de ponto de coleta.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,12 +10,50 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: 'http://localhost:3333/uploads/${item.image}',
         };
     });
 
     return response.json(serializedItems)
+});
+// Criação de ponto de Coleta. Post -> Criar
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const insertedIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    });
+
+    await knex('point_items').insert(pointItems);
+
+    return response.json({ sucess: true});
 });
 
 export default routes;
