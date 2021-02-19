504fc7eba04b8b458c7105914045376134ba8734
Pedro Henrique <46195037+pedrob14@users.noreply.github.com>
2021-02-19 20:43:08 -0300

    React Native

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+
+  },
+});
